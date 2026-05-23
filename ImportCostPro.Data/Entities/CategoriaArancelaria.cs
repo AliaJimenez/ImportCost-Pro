bdc6df7f6317f4c30566389e1e6e86d92ed4df40
Waldin Ceballos Ramirez <smiwaldin@gmail.com>
2026-05-23 00:10:24 -0400

    Cambios locales categoriaArancelaria

--- ImportCostPro.Data/Entities/CategoriaArancelaria.cs
+++ ImportCostPro.Data/Entities/CategoriaArancelaria.cs
@@ -6,7 +6,7 @@ namespace ImportCostPro.Data.Entities
 {
     public class CategoriaArancelaria
     {
-        /*public int Id { get; set; }
+        public int Id { get; set; }
 
         [MaxLength(20)]
         public required string CodigoArancelario { get; set; }
